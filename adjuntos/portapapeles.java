010a747f041ac5b69a7789a7447713f26b6b0bbe
DavidRDigan <159374481+DavidRDigan@users.noreply.github.com>
2025-09-09 16:54:48 -0300

    Archivo inicio de carpeta
    Archivo portapapeles de ejemplos

---
+++ adjuntos/portapapeles.java
@@ -0,0 +1,18 @@
+// Archivo incio portapapeles
+
+// Inicia la clase
+public class ObjectUno(){
+
+// Concatenar un String
+  public String toString(){
+      return variable + " " + variable;
+  }
+// Metodo de comparacion que devuelve "0"
+  public Variable(String nombre){
+    if(nombre != null && !nombre.trim().isEmpty()){
+      this.variable = variable;  
+    }
+  }
+
+
+}
